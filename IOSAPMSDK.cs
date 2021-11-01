16c884cb41ced2575c37c9792a723826dec2d1eb
王玮彬 <wangweibin1@kingsoft.com>
2021-11-01 11:57:20 +0800

    no message

--- IOSAPMSDK.cs
+++ IOSAPMSDK.cs
@@ -6,19 +6,19 @@ using System.Runtime.InteropServices;
 public class IOSAPMSDK : MonoBehaviour
 {
     [DllImport("__Internal")]
-    internal extern static float GetProcessUsedCPU();         //返回当前进程占用的cpu
+    internal extern static float GetProcessUsedCPU();         //返回当前进程占用的cpu, %
     [DllImport("__Internal")]
-    internal extern static float GetProcessUsedMemory();      //返回当前进程使用的内存
+    internal extern static float GetProcessUsedMemory();      //返回当前进程使用的内存, 单位M
     [DllImport("__Internal")]
-    internal extern static float GetDeviceTotalMemory();      //返回设备总的内存
+    internal extern static float GetDeviceTotalMemory();      //返回设备总的内存, 单位M
     [DllImport("__Internal")]
-    internal extern static float GetDeviceFreeMemory();       //返回设备剩余的内存
+    internal extern static float GetDeviceFreeMemory();       //返回设备剩余的内存, 单位M
     [DllImport("__Internal")]
-    internal extern static float GetDeviceUsedMemory();       //返回设备已经使用的内存
+    internal extern static float GetDeviceUsedMemory();       //返回设备已经使用的内存, 单位M
     [DllImport("__Internal")]
-    internal extern static double GetBatteryLevel();          //返回当前电池电量
+    internal extern static double GetBatteryLevel();          //返回当前电池电量，%
     [DllImport("__Internal")]
-    internal extern static float GetDeviceAvailableMemory();  //该接口返回的数值等于设备总内存减去脏内存的大小,仅适用于ios 13.0以上的版本
+    internal extern static float GetDeviceAvailableMemory();  //该接口返回的数值等于设备总内存减去脏内存的大小,仅适用于ios 13.0以上的版本， M
 
     // Start is called before the first frame update
     void Start()
